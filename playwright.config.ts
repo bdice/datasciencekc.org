27249af95e6ac7677aef31a3f5d7771d41c91d34
Bradley Dice <bdice@bradleydice.com>
2026-08-21 18:33:36 -0500

    Hide homepage announcements after configured date

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,12 +13,21 @@ export default defineConfig({
     screenshot: "only-on-failure",
     video: "retain-on-failure"
   },
-  webServer: {
-    command: "hugo server -D --bind 127.0.0.1 --port 1313 --baseURL http://127.0.0.1:1313 --disableFastRender",
-    url: "http://127.0.0.1:1313",
-    reuseExistingServer: !process.env.CI,
-    timeout: 120000
-  },
+  webServer: [
+    {
+      command: "hugo server -D --renderToMemory --bind 127.0.0.1 --port 1313 --baseURL http://127.0.0.1:1313 --disableFastRender",
+      url: "http://127.0.0.1:1313",
+      reuseExistingServer: !process.env.CI,
+      timeout: 120000
+    },
+    {
+      command:
+        "hugo server -D --renderToMemory --clock 2038-01-01T00:00:00-06:00 --bind 127.0.0.1 --port 1314 --baseURL http://127.0.0.1:1314 --disableFastRender",
+      url: "http://127.0.0.1:1314",
+      reuseExistingServer: !process.env.CI,
+      timeout: 120000
+    }
+  ],
   projects: [
     {
       name: "chromium",
